a7afa44b35a79d5d97771cdd7ed4e65db77b9804
tintinsn <tin.sn98@gmail.com>
2023-11-23 13:08:12 +0700

    add: dto of create

--- src/types/dto.tsx
+++ src/types/dto.tsx
@@ -12,3 +12,60 @@ export interface RegisterDTO {
   password: string
   email: string
 }
+
+export interface CreatePortfolioDTO {
+  portfolioInfo: PortfolioInfoDTO
+  education: EducationDTO[]
+  workExperience: WorkExperienceDTO[]
+  project: ProjectDTO[]
+  skill: SkillsDTO[]
+  template?: TemplateDTO[]
+}
+
+export interface PortfolioInfoDTO {
+  portfolioName: string
+  titleName: string
+  profilePicture?: File | null
+}
+
+export interface EducationDTO {
+  id: string
+  school: string
+  degree: string
+  major: string
+  startDate: Date
+  endDate: Date
+}
+export interface WorkExperienceDTO {
+  id:string
+  position: string
+  employeeType: string
+  companyName: string
+  companyLocation: string
+  startDate: Date
+  endDate: Date
+}
+export interface ProjectDTO {
+  id: string
+  title: string
+  detail: string
+  // picture: string
+  category: { name: string }[]
+  tag: { name: string }[]
+  linkProject: string
+  linkGitRepo: string
+}
+
+export interface TemplateDTO {
+  id: number
+  name: string
+  url: string
+}
+export interface SkillItem {
+  // name: string
+}
+
+export interface SkillsDTO {
+  // skills: SkillItem[]
+  name: string
+}
